96812a84375f2c598c8ed6380995d94156a7a580
Rostyslavderii <105818935+Rostyslavderii@users.noreply.github.com>
2023-04-13 12:45:52 +0300

    delete less

--- src/components/HeartButton/HeartButtonLink.jsx
+++ src/components/HeartButton/HeartButtonLink.jsx
@@ -2,21 +2,15 @@ import { BiHeart } from 'react-icons/bi';
 import { FavoriteLink } from './HeartButtonLink.styled';
 import { cartItemsVar } from 'apollo/cache';
 import { useLazyQuery } from '@apollo/client';
-import { GET_ROCKET, GET_ROCKET_ITEMS } from 'apollo/apolloAPI';
+import { GET_ROCKET } from 'apollo/apolloAPI';
 
-import { useReactiveVar, useApolloClient } from '@apollo/client';
+import { useApolloClient } from '@apollo/client';
 import { useContext } from 'react';
 import { cardContext } from 'apollo/useContext';
 
 import { useNavigation } from 'react-router';
 
 export const HeartButtonLink = ({ id, description, name }) => {
-  const client = useApolloClient();
-  const navigation = useNavigation();
-
-  const [GetRocket, { data, loading, error }] = useLazyQuery(GET_ROCKET, {
-    variables: { rocketId: id },
-  });
   const { getCard, card } = useContext(cardContext);
   // console.log(card, 'context');
 
